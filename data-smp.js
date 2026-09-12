6c5a385fb243ff35063b52c57938a929a1621900
scurity-svg <scurity36@gmail.com>
2026-09-12 07:17:38 +0700

    Create data-smp.js

---
+++ data-smp.js
@@ -0,0 +1,115 @@
+/* ZARRLEARN v1.5 - DATA SMP */
+
+var bankSoalSMP = {
+  7: {
+    mtk: [
+      { q: '2x + 5 = 15, x?', o: ['3','4','5','6'], a: 2 },
+      { q: '(-3) x (-4)?', o: ['-12','-7','7','12'], a: 3 },
+      { q: 'Akar 225?', o: ['13','14','15','16'], a: 2 },
+      { q: '2 pangkat 5?', o: ['16','32','64','128'], a: 1 },
+      { q: 'FPB 24 dan 36?', o: ['6','8','12','18'], a: 2 }
+    ],
+    ipa: [
+      { q: 'Satuan panjang SI?', o: ['cm','m','km','mm'], a: 1 },
+      { q: 'Besaran pokok?', o: ['Kecepatan','Massa','Gaya','Tekanan'], a: 1 },
+      { q: 'Alat ukur massa?', o: ['Termometer','Neraca','Barometer','Amperemeter'], a: 1 },
+      { q: 'Gas jadi cair?', o: ['Menguap','Mengembun','Menyublim','Membeku'], a: 1 },
+      { q: 'Suhu tubuh normal?', o: ['35C','36,5C','38C','40C'], a: 1 }
+    ],
+    ips: [
+      { q: 'Ibu kota Indonesia?', o: ['Bandung','Surabaya','Jakarta','Medan'], a: 2 },
+      { q: 'Proklamasi tahun?', o: ['1942','1945','1949','1950'], a: 1 },
+      { q: 'Benua terbesar?', o: ['Afrika','Asia','Eropa','Amerika'], a: 1 },
+      { q: 'Presiden pertama RI?', o: ['Soeharto','Soekarno','Habibie','Megawati'], a: 1 },
+      { q: 'Samudra terluas?', o: ['Atlantik','Hindia','Pasifik','Arktik'], a: 2 }
+    ],
+    bindo: [
+      { q: 'Kata baku?', o: ['Aktifitas','Aktivitas','Aktipitas','Aktifiti'], a: 1 },
+      { q: 'Teks deskripsi berisi?', o: ['Cerita','Gambaran objek','Argumen','Langkah'], a: 1 },
+      { q: 'Sinonim indah?', o: ['Buruk','Cantik','Jelek','Kotor'], a: 1 },
+      { q: 'Kalimat langsung pakai?', o: ['Tanda petik','Kurung','Koma','Titik'], a: 0 },
+      { q: 'Puisi modern disebut?', o: ['Pantun','Puisi bebas','Syair','Gurindam'], a: 1 }
+    ],
+    bing: [
+      { q: 'She ...', o: ['go','goes','going','went'], a: 1 },
+      { q: 'Kemarin?', o: ['Today','Yesterday','Tomorrow','Now'], a: 1 },
+      { q: 'Preposition di atas?', o: ['in','on','at','under'], a: 1 },
+      { q: 'Past buy?', o: ['Buyed','Bought','Buys','Buying'], a: 1 },
+      { q: 'Buku itu milik saya?', o: ['That is my book','That book my','That my book','My that book'], a: 0 }
+    ]
+  },
+  8: {
+    mtk: [
+      { q: 'Rumus Pythagoras?', o: ['a+b=c','a2+b2=c2','a-b=c','ab=c2'], a: 1 },
+      { q: 'x2 = 49, x?', o: ['+-5','+-6','+-7','+-8'], a: 2 },
+      { q: 'Gradien y=3x+2?', o: ['1','2','3','4'], a: 2 },
+      { q: '3x-2y jika x=4 y=3?', o: ['4','5','6','7'], a: 2 },
+      { q: 'Luas lingkaran r=10?', o: ['214','314','414','514'], a: 1 }
+    ],
+    ipa: [
+      { q: 'Hukum Newton I?', o: ['Aksi-reaksi','Kelembaman','F=ma','Gravitasi'], a: 1 },
+      { q: 'Satuan usaha?', o: ['Newton','Joule','Watt','Pascal'], a: 1 },
+      { q: 'Cermin datar sifat?', o: ['Nyata','Maya','Terbalik','Diperbesar'], a: 1 },
+      { q: 'Energi kinetik?', o: ['mgh','1/2 mv2','ma','Fd'], a: 1 },
+      { q: 'Gravitasi Bumi?', o: ['8,9','9,8','10,8','11'], a: 1 }
+    ],
+    ips: [
+      { q: 'ASEAN berdiri?', o: ['1965','1967','1970','1975'], a: 1 },
+      { q: 'Bukan ASEAN?', o: ['Malaysia','Thailand','India','Vietnam'], a: 2 },
+      { q: 'Mata uang Jepang?', o: ['Won','Yuan','Yen','Baht'], a: 2 },
+      { q: 'Gunung tertinggi?', o: ['Fuji','Everest','Kilimanjaro','Semeru'], a: 1 },
+      { q: 'Sungai terpanjang?', o: ['Amazon','Nil','Yangtze','Mississippi'], a: 1 }
+    ],
+    bindo: [
+      { q: 'Teks eksposisi?', o: ['Cerita','Argumen','Gambaran','Langkah'], a: 1 },
+      { q: 'Meskipun konjungsi?', o: ['Temporal','Kausal','Konsesif','Final'], a: 2 },
+      { q: 'Puisi Aku karya?', o: ['Chairil Anwar','WS Rendra','Taufik Ismail','Sapardi'], a: 0 },
+      { q: 'Novel karya?', o: ['Fiksi','Non-fiksi','Ilmiah','Sejarah'], a: 0 },
+      { q: 'Objek Budi memakan roti?', o: ['Budi','Memakan','Roti','-'], a: 2 }
+    ],
+    bing: [
+      { q: 'Past continuous?', o: ['I was eating','I eat','I am eat','I will eat'], a: 0 },
+      { q: 'Bisa?', o: ['May','Can','Must','Will'], a: 1 },
+      { q: 'Mereka sedang belajar?', o: ['They study','They are studying','They studied','They will study'], a: 1 },
+      { q: 'Comparative good?', o: ['Gooder','Better','Best','More good'], a: 1 },
+      { q: 'Apakah kamu suka?', o: ['Do you like?','Are you like?','You like?','You are like?'], a: 0 }
+    ]
+  },
+  9: {
+    mtk: [
+      { q: 'akar144 + akar81?', o: ['19','21','23','25'], a: 1 },
+      { q: '3x-7=14, x?', o: ['5','6','7','8'], a: 2 },
+      { q: 'Sudut segi-6?', o: ['540','720','900','1080'], a: 1 },
+      { q: '5! / 3!?', o: ['10','20','30','40'], a: 1 },
+      { q: 'sin 30?', o: ['0','0,5','0,707','1'], a: 1 }
+    ],
+    ipa: [
+      { q: 'Satuan arus?', o: ['Volt','Ampere','Ohm','Watt'], a: 1 },
+      { q: 'Hukum Ohm?', o: ['V=IR','V=I/R','V=R/I','V=I2R'], a: 0 },
+      { q: 'Alat ukur arus?', o: ['Voltmeter','Amperemeter','Ohm meter','Wattmeter'], a: 1 },
+      { q: 'Pembelahan tubuh?', o: ['Mitosis','Meiosis','Amitosis','Fisi'], a: 0 },
+      { q: 'Kutub magnet sama?', o: ['Tarik','Tolak','Diam','Netral'], a: 1 }
+    ],
+    ips: [
+      { q: 'PD II berakhir?', o: ['1943','1944','1945','1946'], a: 2 },
+      { q: 'PBB berdiri?', o: ['1943','1944','1945','1946'], a: 2 },
+      { q: 'Reformasi?', o: ['1995','1996','1997','1998'], a: 3 },
+      { q: 'Mata uang Eropa?', o: ['Dollar','Euro','Pound','Yen'], a: 1 },
+      { q: 'OPEC bidang?', o: ['Minyak','Pangan','Kesehatan','Pendidikan'], a: 0 }
+    ],
+    bindo: [
+      { q: 'Karya dialog?', o: ['Puisi','Drama','Novel','Cerpen'], a: 1 },
+      { q: 'Struktur prosedur?', o: ['Tujuan-langkah-penutup','Pembuka-isi','Awal-akhir','Intro-klimaks'], a: 0 },
+      { q: 'Namun konjungsi?', o: ['Penambahan','Pertentangan','Pilihan','Sebab'], a: 1 },
+      { q: 'Terbaik termasuk?', o: ['Superlatif','Komparatif','Positif','Negatif'], a: 0 },
+      { q: 'Pidato bertujuan?', o: ['Menghibur','Meyakinkan','Menjelaskan','Bercerita'], a: 1 }
+    ],
+    bing: [
+      { q: 'Present perfect?', o: ['I have eaten','I ate','I eat','I will eat'], a: 0 },
+      { q: 'Harus?', o: ['May','Must','Can','Will'], a: 1 },
+      { q: 'Passive buku dibaca Budi?', o: ['Budi reads','Book is read by Budi','Budi reading','Book reads'], a: 1 },
+      { q: 'Superlative big?', o: ['Bigger','Biggest','More big','Most big'], a: 1 },
+      { q: 'Sudah tinggal 5 tahun?', o: ['I live here','I have lived here for 5 years','I lived','I am living'], a: 1 }
+    ]
+  }
+};
